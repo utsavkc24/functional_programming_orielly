2eaab9cb4f2cd3994ff8ada7579930cb2f0b836d
Utsav Kumar <utsavkc24@gmail.com>
2024-06-14 13:05:17 +0530

    looking at concurrency

--- src/main/java/org/example/CollectAverage.java
+++ src/main/java/org/example/CollectAverage.java
@@ -27,12 +27,26 @@ class Averager {
 
 public class CollectAverage {
     public static void main(String[] args) {
+        long start = System.nanoTime();
         Averager result = DoubleStream.generate(() -> ThreadLocalRandom.current().nextDouble(-Math.PI, Math.PI))
-                .limit(1_000)
+                .parallel()
+                .limit(4_000_000_000L)
                 .collect(() -> new Averager(),
                         (b, i) -> b.include(i),
                         (b1, b2) -> b1.merge(b2));
 
-        System.out.println("Average is " + result.get());
+        long end = System.nanoTime();
+        System.out.println("Average is " + result.get() + " - computation took " + (end - start) / 1_000_000 + " ms");
+
+
+        long start_non_parallel = System.nanoTime();
+        Averager result_non_parallel = DoubleStream.generate(() -> ThreadLocalRandom.current().nextDouble(-Math.PI, Math.PI))
+                .limit(4_000_000_000L)
+                .collect(() -> new Averager(),
+                        (b, i) -> b.include(i),
+                        (b1, b2) -> b1.merge(b2));
+
+        long end_non_parallel = System.nanoTime();
+        System.out.println("Average is " + result_non_parallel.get() + " - computation took " + (end_non_parallel - start_non_parallel) / 1_000_000 + " ms");
     }
 }
